9cf4738629a550ebe3f4e19f53f3134bc31ab148
Deac Karns <deac@sfp.net>
2018-04-27 16:05:44 -0400

    remove cruft; do we even need this

--- src/EventCollection.php
+++ src/EventCollection.php
@@ -1,32 +1,7 @@
 <?php
 namespace KO\Fullcalendar;
 
-class EventCollection
+class EventCollection extends \Illuminate\Database\Eloquent\Collection
 {
-
-    /**
-     * @var Collection
-     */
-    protected $events;
-
-    public function __construct()
-    {
-        $this->events = collect([]);
-    }
-
-    public function push(Event $event)
-    {
-        $this->events->push( $event );
-    }
-
-    public function toJson()
-    {
-        return $this->events->toJson();
-    }
-
-    public function toArray()
-    {
-        return $this->events->toArray();
-    }
 
 }
