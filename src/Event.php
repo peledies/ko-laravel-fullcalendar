cd9dc8ed6b72de0c9826ab250709bf24c8f1a9b0
Deac Karns <deac@sfp.net>
2018-04-27 15:20:01 -0400

    building event model

--- src/Event.php
+++ src/Event.php
@@ -43,81 +43,39 @@ class Event implements Interfaces\Event
      */
     private $options;
 
-    /**
-     * @param string          $title
-     * @param bool            $isAllDay
-     * @param string|DateTime $start If string, must be valid datetime format: http://bit.ly/1z7QWbg
-     * @param string|DateTime $end   If string, must be valid datetime format: http://bit.ly/1z7QWbg
-     * @param int|string|null $id
-     * @param array           $options
-     */
-    public function __construct($title, $isAllDay, $start, $end, $id = null, $options = [])
+    // these fields get converted to carbon dates
+    private $dates = [
+          'start'
+        , 'end'
+    ];
+
+    public function __construct(array $defaults = null)
     {
-        $this->title    = $title;
-        $this->isAllDay = $isAllDay;
-        $this->start    = $start instanceof DateTime ? $start : new DateTime($start);
-        $this->end      = $start instanceof DateTime ? $end : new DateTime($end);
-        $this->id       = $id;
-        $this->options  = $options;
+        if($defaults){
+            $this->fill( $defaults );
+        }
+    }
+
+    public function fill( $data )
+    {
+        foreach ($data as $key => $value) {
+            if( property_exists($this, $key) ){
+                if( in_array($key, $this->dates) ){
+                    $this->$key = ( $value instanceof \Carbon\Carbon ) ? $value : new \Carbon\Carbon($value);
+                }else{
+                    $this->$key = $value;
+                }
+            }
+        }
     }
 
     /**
-     * Get the event's id number
+     * Get a class property by name
      *
      * @return int
      */
-    public function getId()
+    public function get( $property )
     {
-        return $this->id;
-    }
-
-    /**
-     * Get the event's title
-     *
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
-     * Is it an all day event?
-     *
-     * @return bool
-     */
-    public function isAllDay()
-    {
-        return $this->isAllDay;
-    }
-
-    /**
-     * Get the start time
-     *
-     * @return DateTime
-     */
-    public function getStart()
-    {
-        return $this->start;
-    }
-
-    /**
-     * Get the end time
-     *
-     * @return DateTime
-     */
-    public function getEnd()
-    {
-        return $this->end;
-    }
-
-    /**
-     * Get the optional event options
-     *
-     * @return array
-     */
-    public function getEventOptions()
-    {
-        return $this->options;
+        return $this->$property;
     }
 }
